e278316a9de149d94375a7ebd556407228207529
Jeromy <jkanglim@gmail.com>
2019-06-10 16:30:05 +1000

    updated for 2019

--- project-examples/3-simple-project-template-example/created-example/lib/data-import-override.r
+++ project-examples/3-simple-project-template-example/created-example/lib/data-import-override.r
@@ -1,2 +1,10 @@
 # Add alternative data importation commands for files in 
 # the data folder that should not be imported using standard data importation rules
+
+
+# Sometime around version 0.8.2, tibble conversion was made d
+# disable tibble conversion
+
+try({.convert.to.tibble <- function (data.sets)  NULL;
+    assignInNamespace(".convert.to.tibble", .convert.to.tibble, 
+                  "ProjectTemplate")})
